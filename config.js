5674eb9916d034574dcf00089e4ff495c1ccba71
rolaxxx3 <ruslannightcore@outlook.com>
2020-10-18 22:00:13 +0300

    fix config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
-    URL: process.env.BASE_URL || 'http://localhost:3000',
     MONGODB_URI: getDatabaseUri(),
 };
 
