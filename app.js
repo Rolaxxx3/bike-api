bf7f2b5f71dac07abc70dc911cf7105df6394894
rolaxxx3 <ruslannightcore@outlook.com>
2020-10-20 15:10:45 +0300

    fix CORS

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Origin, X-Requested-With, Content-Type, Accept"
     );
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PATCH GET POST DELETE');
